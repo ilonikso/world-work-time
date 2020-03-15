4e479b8322edddfc32d65c6ab681160aa2c2c5d0
Illia Onishchenko <pearljam546@gmail.com>
2020-03-15 03:31:42 -0700

    Fix Time zone cross function

--- source/js/app.js
+++ source/js/app.js
@@ -22,46 +22,21 @@ const nowTime = {
     getDateLineCross: function(cityUTCOffset){
         let LocalUTCOffset = -this.getUTCOffset();
         
-
-        if((cityUTCOffset < LocalUTCOffset) && (cityUTCOffset < 0) && (LocalUTCOffset - cityUTCOffset > LocalUTCOffset ) && (LocalUTCOffset > 0)){
-            //console.log('cross forward');
-
-            return 1;
-        }   
-        
-        if((cityUTCOffset > LocalUTCOffset) && (cityUTCOffset > 0) && (LocalUTCOffset - cityUTCOffset < LocalUTCOffset ) && (LocalUTCOffset < 0)){
-            //console.log('cross back');
-
-            return -1;
+        if(cityUTCOffset < 0 && cityUTCOffset !== LocalUTCOffset){
+            return 1;    
         }
 
         return 0
     },
 
-    getDateLineCross2: function(loc, city){
-        console.log(' *** Cross function test ***');
-
-        
-        if((city < loc) && (city < 0) && (loc - city > loc ) && (loc > 0)){
-            console.log('cross forward');
-        }
-
-        if((city > loc) && (city > 0) && (loc - city < loc ) && (loc < 0)){
-            console.log('cross back');
-
-        }
-
-
-        console.log(' *** Cross function test ***')
-    },
     getLocalTime: function(){
         return this.getHours() + ':' + this.getMinutes() + ':' + this.getSeconds();
     },
     getTime: function(offset = 0){
         
         //Offset limit
-        offset >= 12 ? offset = 12 : offset;
-        offset <= -12 ? offset = -12 : offset;
+        offset >= 12 ? offset -= 24 : offset;
+        offset <= -12 ? offset += 24 : offset;
 
         // Check hour > 24
         let hours = this.getHours() + offset;
@@ -131,6 +106,8 @@ console.log('New York time | ' + nowTime.getTime(nowTime.getUTCOffset() - 5 + 1)
 console.log('Los Angeles time | ' + nowTime.getTime(nowTime.getUTCOffset() - 8 + 1));
 
 console.log('Tokio time | ' + nowTime.getTime(nowTime.getUTCOffset() + 9 ));
+
+console.log('Kiev time | ' + nowTime.getTime(nowTime.getUTCOffset() + 2 ));
 
 cities.forEach(item => {
     console.log(`--- ${item.name} time | ${nowTime.getTime(nowTime.getUTCOffset() + item.UTCOffset + nowTime.getDateLineCross(item.UTCOffset) ) }`);
