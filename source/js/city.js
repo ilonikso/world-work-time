cc5fdcbf52cf14d8228bb04b31f8c4b0fc1cee9e
Illia Onishchenko <pearljam546@gmail.com>
2020-03-19 15:29:18 +0300

    Format files

--- source/js/city.js
+++ source/js/city.js
@@ -1,49 +1,46 @@
-class City{
-    constructor(name, UTCOffset, id){
-        this.name = name || 'UTC Time';
+class City {
+    constructor(name, UTCOffset, id) {
+        this.name = name || "UTC Time";
         this.UTCOffset = UTCOffset || 0;
         this.id = 0 || id;
     }
 }
 
-class Validator{
-    constructor(){
+class Validator {
+    constructor() {}
 
-    }
-
-    validateName(name){
+    validateName(name) {
         const error = name.nextElementSibling;
         const re = /^([A-Za-zа-яА-Я]{2,10})?[ .-]+([A-Za-zа-яА-Я]{2,10})|([A-Za-zа-яА-Я]{2,10})$/;
-        
-        if(!re.test(name.value)){
-        name.classList.add('is-invalid');
-        error.style.visibility = 'visible';
-        
-        return false;
-        } else{
-        name.classList.remove('is-invalid');
-        error.style.visibility = 'hidden';
-        
-        return true;
+
+        if (!re.test(name.value)) {
+            name.classList.add("is-invalid");
+            error.style.visibility = "visible";
+
+            return false;
+        } else {
+            name.classList.remove("is-invalid");
+            error.style.visibility = "hidden";
+
+            return true;
         }
     }
 
-    validateOffset(offset){
+    validateOffset(offset) {
         const zip = offset;
         const error = zip.nextElementSibling;
         const re = /^[0-9]{2}$/;
-        
-        if(+zip.value >= -12 && +zip.value <= 12){
-            zip.classList.remove('is-invalid');
-            error.style.visibility = 'hidden';
-            
+
+        if (+zip.value >= -12 && +zip.value <= 12) {
+            zip.classList.remove("is-invalid");
+            error.style.visibility = "hidden";
+
             return true;
         } else {
-            zip.classList.add('is-invalid');
-            error.style.visibility = 'visible';
-            
+            zip.classList.add("is-invalid");
+            error.style.visibility = "visible";
+
             return false;
         }
     }
 }
-
